e0f5ded385ec930a2ad6637fbb18a0d13afd0791
jmorris-pillar <jmorris2@pillartechnology.com>
2020-03-19 15:11:03 -0400

    Cloudformation worldwide
    Allow a single cloud formation script to create multiple environments parametrically.
    Save secrets/configurations of redis and metastore to secrets manager so these can be retrieved programatically.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 function deploy {
-    local -r bucket="${1:?Bucket name required.}"
-    shift
+
+    local -r password=$(openssl rand 24 -hex)
 
     aws cloudformation deploy \
-        --stack-name=${STACK_NAME} \
+        --stack-name=${STACK_PREFIX}-${ENVIRONMENT_NAME} \
         --template-file hindsight.yaml \
         --capabilities CAPABILITY_NAMED_IAM \
-        --parameter-overrides DataBucketName=${bucket} \
+        --parameter-overrides \
+            DataBucketPrefix=${BUCKET_PREFIX} \
+            EnvironmentName=${ENVIRONMENT_NAME} \
+            DbPassword=${password} \
         "${@}"
 }
 
@@ -16,7 +21,7 @@ function get_output {
     local -r key="${1}"
 
     aws cloudformation describe-stacks \
-        | jq -r ".Stacks[] | select(.StackName == \"${STACK_NAME}\") | .Outputs[] | select(.OutputKey == \"${key}\") | .OutputValue"
+        | jq -r ".Stacks[] | select(.StackName == \"${STACK_PREFIX}-${ENVIRONMENT_NAME}\") | .Outputs[] | select(.OutputKey == \"${key}\") | .OutputValue"
 }
 
 function iam_mapping {
@@ -26,13 +31,32 @@ function iam_mapping {
     helm template aws ./helm --set aws.role.node="${node}",aws.role.user="${user}" | kubectl apply -n kube-system -f -
 }
 
+function get_kubeconfig {
+    eksctl utils write-kubeconfig hindsight-kubernetes-${ENVIRONMENT_NAME}
+}
+
+function check_dependencies {
+    type eksctl 1> /dev/null
+    type openssl 1> /dev/null
+    type helm 1> /dev/null
+    type jq 1> /dev/null
+}
+
 if [[ $1 == "-h" || $1 == "--help" ]]; then
-    echo "Usage: ./deploy.sh [STACK_NAME] [BUCKET_NAME] [cf_flags]"
+    echo "Usage: ./deploy.sh [STACK_PREFIX] [BUCKET_PREFIX] [ENVIRONMENT_NAME] [cf_flags]"
     exit 0
 fi
 
-declare -r STACK_NAME="${1:?Stack name required.}"
+declare -r STACK_PREFIX="${1:?Stack prefix required.}"
 shift
 
+declare -r BUCKET_PREFIX="${1:?Bucket prefix required.}"
+shift
+
+declare -r ENVIRONMENT_NAME="${1:?Environment name required.}"
+shift
+
+check_dependencies
 deploy $@
+get_kubeconfig
 iam_mapping
